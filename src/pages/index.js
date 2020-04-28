a4b99495373a2d07d5c82bddfde4777c9d9b1c90
Arthur Fücher <arthurfucher@gmail.com>
2020-04-27 23:47:59 -0300

    mudança da pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Aloha!</h1>
+    <p>Esse é o meu super blog!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
