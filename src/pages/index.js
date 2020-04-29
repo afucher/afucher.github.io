5eeec761b9f6e71d3d18c58d8f3b3beea9f6f1fd
Arthur Fücher <arthurfucher@gmail.com>
2020-04-28 23:48:23 -0300

    cria as paginas do post baseado no markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({data}) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Aloha!</h1>
-    <p>Esse é o meu super blog! Que tem {data.allMarkdownRemark.totalCount} posts.</p>
-    <ul>
-      {data.allMarkdownRemark
-            .edges
-            .map(edge => <li>{`${edge.node.frontmatter.title} - ${edge.node.frontmatter.date}`}</li>)}
-    </ul>
+const IndexPage = ({data}) => {
+  const posts = data.allMarkdownRemark.edges;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Aloha!</h1>
+      <p>Esse é o meu super blog! Que tem {data.allMarkdownRemark.totalCount} posts.</p>
+      <ul>
+        {posts.map(({node}) => <li>
+                                <Link to={node.fields.slug}>
+                                  {`${node.frontmatter.title} - ${node.frontmatter.date}`}
+                                </Link>
+                              </li>)}
+      </ul>
 
-  </Layout>
-)
+    </Layout>
+)}
 
 export const query = graphql`
   query MyQuery {
@@ -26,6 +30,9 @@ export const query = graphql`
           frontmatter {
             date(formatString: "DD/MM/YYYY")
             title
+          }
+          fields {
+            slug
           }
         }
       }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+
+export default ({data}) => {
+    return <Layout>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+    </Layout>
+}
+
+export const query = graphql`
+    query($slug: String) {
+        markdownRemark(fields: {slug: {eq: $slug}}) {
+            frontmatter {
+                title
+            }
+            html
+        }
+    }
+`
